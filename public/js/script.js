de6c97b7f3ededb3710b2bad4dd3aeb35617b841
Ayoub BAYED <ayoub@code42.fr>
2024-02-25 13:03:03 +0100

    [#?] Added: two new experiences

--- public/js/script.js
+++ public/js/script.js
@@ -1,53 +1,66 @@
-$(document).ready(function() {
-    $("#languages,#competences,#formations").hide();
-    
-    $("#Formations").click(function() {
-        $(this).addClass("active");
-        $("#Journal,#Compétences,#Langues").removeClass("active");
-        $("#formations").show();
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline,#languages,#competences").hide();
-    });
+$(document).ready(function () {
+  $("#languages,#competences,#formations").hide();
 
-    $("#Journal").click(function() {
-        $(this).addClass("active");
-        $("#Formations,#Compétences,#Langues").removeClass("active");
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline").show();
-        $("#formations,#competences,#languages").hide();
-    });
+  $("#Formations").click(function () {
+    $(this).addClass("active");
+    $("#Journal,#Compétences,#Langues").removeClass("active");
+    $("#formations").show();
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline,#languages,#competences"
+    ).hide();
+  });
 
-    $("#Compétences").click(function() {
-        $(this).addClass("active");
-        $("#Formations,#Journal,#Langues").removeClass("active");
-        $("#competences, #front").show();
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline,#languages,#formations,#deploy,#back").hide();
-    });
+  $("#Journal").click(function () {
+    $(this).addClass("active");
+    $("#Formations,#Compétences,#Langues").removeClass("active");
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline"
+    ).show();
+    $("#formations,#competences,#languages").hide();
+  });
 
-    $("#Langues").click(function() {
-        $(this).addClass("active");
-        $("#Formations,#Journal,#Compétences").removeClass("active");
-        $("#languages").show();
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline,#competences,#formations").hide();
-    });
+  $("#Compétences").click(function () {
+    $(this).addClass("active");
+    $("#Formations,#Journal,#Langues").removeClass("active");
+    $("#competences, #front").show();
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline,#languages,#formations,#deploy,#back"
+    ).hide();
+  });
 
-    $("#backend").click(function() {
-        $(this).addClass("active");
-        $("#frontend, #deployment").removeClass("active");
-        $("#back").show();
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline,#formations, #deploy,#front").hide();
-    });
+  $("#Langues").click(function () {
+    $(this).addClass("active");
+    $("#Formations,#Journal,#Compétences").removeClass("active");
+    $("#languages").show();
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline,#competences,#formations"
+    ).hide();
+  });
 
-    $("#deployment").click(function() {
-        $(this).addClass("active");
-        $("#frontend, #backend").removeClass("active");
-        $("#deploy").show();
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline,#formations, #back,#front").hide();
-    });
+  $("#backend").click(function () {
+    $(this).addClass("active");
+    $("#frontend, #deployment").removeClass("active");
+    $("#back").show();
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline,#formations, #deploy,#front"
+    ).hide();
+  });
 
-    $("#frontend").click(function() {
-        $(this).addClass("active");
-        $("#deploy, #backend").removeClass("active");
-        $("#front").show();
-        $("#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#timeline,#formations, #back,#deploy").hide();
-    });
+  $("#deployment").click(function () {
+    $(this).addClass("active");
+    $("#frontend, #backend").removeClass("active");
+    $("#deploy").show();
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline,#formations, #back,#front"
+    ).hide();
+  });
 
+  $("#frontend").click(function () {
+    $(this).addClass("active");
+    $("#deploy, #backend").removeClass("active");
+    $("#front").show();
+    $(
+      "#professional1,#professional2,#professional3,#professional4,#professional5,#professional6,#professional7,#professional8,#professional9,#professional10,#timeline,#formations, #back,#deploy"
+    ).hide();
+  });
 });
